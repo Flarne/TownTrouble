11be654e6d310686d0788c0ecd2d0a365ad06776
Ulrik <ulrik.bergstrom@gmail.com>
2019-08-23 12:55:43 +0200

    Added a weapon switch script. Made some changes on weapon and DeathHandler scripts too

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+	[SerializeField] int currentWeapon = 0;
+
+    void Start()
+    {
+		SetActiveWeapon();
+	}
+
+	void Update()
+	{
+		int previousWeapon = currentWeapon;
+
+		ProcessKeyboardInput();
+		ProcessScrollWheelInput();
+
+		if (previousWeapon != currentWeapon)
+		{
+			SetActiveWeapon();
+		}
+	}
+
+	private void ProcessKeyboardInput()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			currentWeapon = 0;
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			currentWeapon = 1;
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			currentWeapon = 2;
+		}
+		else
+		{
+			return;
+		}
+	}
+
+	private void ProcessScrollWheelInput()
+	{
+		if (Input.GetAxis("Mouse ScrollWheel") < 0)
+		{
+			if (currentWeapon >= transform.childCount - 1)
+			{
+				currentWeapon = 0;
+			}
+			else
+			{
+				currentWeapon++;
+			}
+		}
+
+		if (Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			if (currentWeapon <= 0)
+			{
+				currentWeapon = transform.childCount - 1;
+			}
+			else
+			{
+				currentWeapon--;
+			}
+		}
+	}
+
+	private void SetActiveWeapon()
+	{
+		int weaponIndex = 0;
+
+		foreach (Transform weapon in transform)
+		{
+			if (weaponIndex == currentWeapon)
+			{
+				weapon.gameObject.SetActive(true);
+			}
+			else if (weaponIndex != currentWeapon)
+			{
+				weapon.gameObject.SetActive(false);
+			}
+			weaponIndex++;
+		}
+	}
+}
