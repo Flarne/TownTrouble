c0add0e823f369907ebbe31c183feb88113a1edc
Ulrik <ulrik.bergstrom@gmail.com>
2019-10-21 13:20:59 +0200

    Began to try out some animations for gates but started to make some lamps instead added Flashlight script and openGate script

---
+++ Assets/Scripts/OpenGate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour
+{
+	public bool enter = true;
+	public bool exit = true;
+	float smooth = 5.0f;
+
+	private void Awake()
+	{
+		//Quaternion gateTarget = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+		//transform.rotation = Quaternion.Slerp(transform.rotation, gateTarget, Time.deltaTime * smooth);
+	}
+
+	//private void Update()
+	//{
+	//	OpeningGate();
+	//}
+
+	//private void OpeningGate()
+	//{
+		
+	//}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (enter)
+		{
+			Debug.Log("entered");
+			GetComponent<Animator>().SetBool("Opening", true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (exit)
+		{
+			Debug.Log("Exited");
+		}
+	}
+}
---
+++ Assets/Scripts/ToggleFlashLight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleFlashLight : MonoBehaviour
+{
+	[SerializeField] Light flashLight;
+
+	public bool toggleOnOff = false;
+
+    void Start()
+    {
+		flashLight = GetComponent<Light>();
+		//flashLight.gameObject.SetActive(false);
+    }
+
+	private void Update()
+	{
+		FlashLightToggleOnOff();
+	}
+
+	public void FlashLightToggleOnOff ()
+	{
+		if (Input.GetKeyDown(KeyCode.L) && toggleOnOff == false)
+		{
+			Debug.Log(flashLight);
+			flashLight.enabled = true;
+			toggleOnOff = true;
+		}
+		else if (Input.GetKeyDown(KeyCode.L) && toggleOnOff == true)
+		{
+			Debug.Log(toggleOnOff);
+			flashLight.enabled= false;
+			toggleOnOff = false;
+		}
+	}
+}
