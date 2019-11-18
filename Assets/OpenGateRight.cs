f3fa40ba244240a88a9be5ed16bc2068bb8355fc
Ulrik <ulrik.bergstrom@gmail.com>
2019-11-18 20:07:07 +0100

    Think I have made it today, animation for gates working as intended

--- Assets/OpenGateRight.cs
+++ Assets/OpenGateRight.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class OpenGateRight : MonoBehaviour
 {
-	[SerializeField] Animator openGateRight;
+	//[SerializeField] Animator openGateRight;
 
-	public bool enterRight = true;
+	//public bool enterRight = true;
 
-	void Start()
-	{
-		openGateRight = GetComponent<Animator>();
-		openGateRight.enabled = false;
-	}
+	//void Start()
+	//{
+	//	openGateRight = GetComponent<Animator>();
+	//	openGateRight.enabled = false;
+	//}
 
-	private void OnTriggerEnter(Collider other)
-	{
-		if (enterRight)
-		{
-			openGateRight.enabled = true;
-			Debug.Log("entered Right");
-			openGateRight.SetBool("openingRight", true);
-			openGateRight.SetBool("openedRight", true);
-		}
-	}
+	//private void OnTriggerEnter(Collider other)
+	//{
+	//	if (enterRight)
+	//	{
+	//		openGateRight.enabled = true;
+	//		Debug.Log("entered Right");
+	//		openGateRight.SetBool("openingRight", true);
+	//		openGateRight.SetBool("openedRight", true);
+	//	}
+	//}
 }
--- Assets/Scripts/OpenGate.cs
+++ Assets/Scripts/OpenGate.cs
@@ -6,50 +6,123 @@ using UnityEngine;
 public class OpenGate : MonoBehaviour
 {
 	[SerializeField] Animator openGate;
-	//[SerializeField] Animator openGateRight;
 
 	public bool enter = true;
+	bool isAnimating = false;
 	//public bool enterRight = true;
 	public bool exit = true;
+	public float gateTime;
 	float smooth = 5.0f;
 
 	private void Start()
 	{
 		openGate = GetComponent<Animator>();
-		//openGateRight = GetComponent<Animator>();
 		openGate.enabled = false;
-		//openGateRight.enabled = false;
+	}
+
+	private void Update()
+	{
+		gateTime += Time.deltaTime;
+
+		if (gateTime <= 10f)
+		{
+			isAnimating = false;
+			return;
+		}
+		else if (gateTime > 10f || gateTime < 12f)
+		{
+			openGate.enabled = false;
+			isAnimating = true;
+			gateTime = 0f;
+		}
+		if (openGate.enabled == false)
+		{
+			isAnimating = true;
+			gateTime = 0f;
+		}
+	}
+
+	public IEnumerator toggleOnOffAnimator()
+	{
+		yield return new WaitForSeconds(4f);
+		openGate.enabled = false;
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (enter)
+		gateTime = 0f;
+		//if (enter && isAnimating && gateTime >= 0 || gateTime <= 5)
+		if (enter && gateTime <= 10f && !isAnimating)
 		{
 			openGate.enabled = true;
-			Debug.Log("entered");
-			openGate.SetBool("opening", true);
-			openGate.SetBool("opened", true);
+			//isAnimating = true;
+			openGate.SetTrigger("openingTriggerRight");
+			openGate.SetBool("openedRight", true);
+			//openGate.SetBool("closing", false);
+			//openGate.SetBool("closingRight", false);
+			//openGate.SetBool("opening", true);
+			//openGate.SetBool("opened", true);
+			//openGate.SetBool("openingRight", true);
+
+			//if (gateTime <= 5f && isAnimating)
+			//{
+			//	return;
+			//}
+			//else if (gateTime > 5f)
+			//{
+			//	Debug.Log(isAnimating);
+			//}
+
+			//else if (!isAnimating && gateTime > 5f)
+			//{
+			//	gateTime = 0f;
+			//	isAnimating = true;
+			//	openGate.enabled = false;
+			//}
 		}
 
 		//if (enterRight)
 		//{
-		//	openGateRight.enabled = true;
+		//	openGate.enabled = true;
 		//	Debug.Log("entered Right");
-		//	openGateRight.SetBool("openingRight", true);
-		//	openGateRight.SetBool("openedRight", true);
+		//	openGate.SetBool("openingRight", true);
+		//	openGate.SetBool("openedRight", true);
 		//}
 	}
 
-			//animator.enabled = true;
-			//animator.SetBool("open", true);
-			//animator.SetBool("opened", true);
-
 	private void OnTriggerExit(Collider other)
 	{
-		if (exit)
+		//if (exit && gateTime >= 0 || gateTime <= 5)
+		if (exit && gateTime <= 10f && !isAnimating)
 		{
-			Debug.Log("Exited");
-			//openGate.enabled = false;
+			gateTime = 0f;
+			openGate.enabled = true;
+			//isAnimating = true;
+			openGate.SetTrigger("closingTriggerRight");
+			openGate.SetBool("closedRight", true);
+			//openGate.SetBool("closingRight", true);
+			//openGate.SetBool("openedRight", false);
+			//openGate.SetBool("openingRight", false);
+			//openGate.SetBool("closing", true);
+			//openGate.SetBool("closed", true);
+			//openGate.SetBool("opening", false);
+
+			//if (gateTime <= 5f && isAnimating)
+			//{
+			//	return;
+			//}
+			//else if (gateTime > 5f)
+			//{
+			//	isAnimating = false;
+			//	Debug.Log(isAnimating);
+			//}
+			//else if (!isAnimating && gateTime > 5f)
+			//{
+			//	gateTime = 0f;
+			//	isAnimating = true;
+			//	//openGate.enabled = false;
+			//}
+			//StartCoroutine(toggleOnOffAnimator());
 		}
 	}
 }
